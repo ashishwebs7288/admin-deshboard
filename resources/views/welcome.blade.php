9fd1e41bb5377d099afc882e8c683d7bd3b30614
ashishwebs7288 <ashish.kumar7288@gmail.com>
2023-04-20 16:46:16 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!-- Content Wrapper. Contains page content -->
+
+  @extends('layouts.app')
+  @section('content')
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+    <h1>My Profile</h1>
+    
+
+  <!-- /.content-wrapper -->
+  @endsection
